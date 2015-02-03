7a2c2d06870d29430a7335cf6909463acd89fdc9
elias_nieto@hotmail.com <abc123>
2015-02-03 13:42:24 +0100

    Codigo terremotos

---
+++ app/src/main/java/com/compostela/curso/terremotosapp/BusquedaFragment.java
@@ -0,0 +1,15 @@
+package com.compostela.curso.terremotosapp;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BusquedaFragment extends Fragment {
+
+
+}
